5d64f818abc88151321f3e49213adf84891f34d0
SAEStudentNathanHoward <114543371+SAEStudentNathanHoward@users.noreply.github.com>
2022-10-12 11:46:52 +1100

    Week 4 CoinSpawner prefab activity

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NathanHoward
+{
+    public class CoinSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject coinPrefab;
+
+        [SerializeField] private Camera mainCamera;
+
+        void Start()
+        {
+            SpawnCoin();
+            SpawnCoin();
+        }
+
+        void SpawnCoin()
+        {
+            int xPosition = Random.Range(-50, 50);
+            int yPosition = Random.Range(-50, 50);
+            int zPosition = Random.Range(-50, 50);
+
+            mainCamera.ViewportToWorldPoint(Vector3 position);
+
+            Instantiate(coinPrefab, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity);
+        }
+        
+    }
+}
